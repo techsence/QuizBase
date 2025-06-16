7c9e4fb1bc9b45e6d512bafce69a41b552ce7a6b
CH989877\admin <developer@techsence.co.jp>
2025-06-17 02:18:24 +0900

    modified scenes

---
+++ Assets/Scripts/Utility/AdManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Utility
+{
+    public static class AdManager
+    {
+        public static void Initialize()
+        {
+        }
+
+        public static void ShowBannerAd()
+        {
+        }
+
+        public static void ShowInterstitialAd()
+        {
+            
+        }
+    }    
+}
+
